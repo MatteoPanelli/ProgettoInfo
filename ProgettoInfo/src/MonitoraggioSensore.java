e821e47678a832af617685d60925cf0a7d87885f
MatteoPanelli <m.panelli005@studenti.unibs.it>
2025-11-07 19:03:17 +0100

    gestione parziale del monitoraggio

---
+++ ProgettoInfo/src/MonitoraggioSensore.java
@@ -0,0 +1,26 @@
+
+public class MonitoraggioSensore {
+		
+	private boolean attivo = false;
+	
+	
+	public synchronized boolean inviaSegnale() {
+		return attivo;
+	}
+	
+	
+	public synchronized void attendiSegnale() {
+		while(!attivo) {
+			try {
+				wait();
+				
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	
+}
