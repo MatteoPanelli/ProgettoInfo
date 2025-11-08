a4f29a92eda7b6eb63826e39459a8357956be3f8
MatteoPanelli <m.panelli005@studenti.unibs.it>
2025-11-08 12:26:33 +0100

    Inizio gestione Sensori

---
+++ ProgettoInfo/src/Sensore.java
@@ -0,0 +1,37 @@
+
+public class Sensore extends Thread {
+	
+	private MonitoraggioSensore monitoaggio;
+	private String sensore;
+	
+	public Sensore(String sensore, MonitoraggioSensore monitoraggio) {
+		this.monitoaggio = monitoraggio;
+		this.sensore = sensore;
+	}
+	
+	
+	@Override 
+	public void run() {
+			
+		while(true) {
+			
+			try {
+				monitoaggio.attendiSegnale();
+				
+				System.out.println(Thread.currentThread().getName() + " -> SBLOCCATO! Misura unica.");
+				
+				Thread.sleep(1000);
+				int misura = (int) (Math.random() * 100);
+				
+				String messaggio = "Sensore " + sensore + " ha misurato il valore: " + misura;
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+
+
